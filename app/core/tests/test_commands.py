7054f7a3b8c4ab0e68f8207baab5e50152173b72
itsyouyouAgain <qing.yo@northeastern.edu>
2024-04-05 18:33:45 -0700

    create changes

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,8 @@
 """
 Test custom Django management commands.
 """
-# mock the behavior of the database because we need to be able to simulate when database is return response or not
+# mock the behavior of the database because we need to be able to simulate 
+# when database is return response or not
 from unittest.mock import patch
 
 # errors we might get when we try and connect to db
@@ -14,6 +15,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 
 from django.test import SimpleTestCase
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
@@ -32,12 +34,10 @@ class CommandTest(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         """This is how mocking work, db was not ready"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect=[Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 6)
+        self.assertEqual(patched_check.call_count,6)
         patched_check.assert_called_with(databases=['default'])
-        
-         
