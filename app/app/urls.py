7f0425f8650c6735c700ee343affddbc86be9685
itsyouyouAgain <qing.yo@northeastern.edu>
2024-04-07 16:17:31 -0700

    add recipe tag api

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,9 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',
+        'api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),name='api-docs',
     ),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,7 +25,7 @@ class AdminSiteTests(TestCase):
         )
 
     # unit test
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
